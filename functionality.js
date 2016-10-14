92261dad405aee5706e8b76d1d5038011ecf0990
Wilson Ng <wilsonhyng@gmail.com>
2016-10-14 15:55:11 -0700

    Fixed bugs - displays new tweets, without duplicates

--- functionality.js
+++ functionality.js
@@ -19,23 +19,108 @@
 
 
 
+// $(document).ready(function() {
+
+//   // var $body = $('body');
+//   // $body.html('');
+
+//   var index = streams.home.length - 1;
+//   while (index >= 0) {
+//     var tweet = streams.home[index];
+//     // var $tweet = $('<div></div>');
+//     var $tweet = $('<div />', {'class': 'tweet'});
+
+//     // make another, use time div / class to style the time $time = $('<div />', {'class': 'time'});
+//     // appendTo row4 after the tweet
+
+//     if (tweet.user === 'shawndrost') {
+
+//       $tweet.text('@' + tweet.user + ': ' + tweet.message + '\ Time: ' + streams.home[index].created_at);
+//       $tweet.appendTo('.row1').hide().fadeIn(1000);
+//     }
+
+//     if (tweet.user === 'sharksforcheap') {
+
+//       $tweet.text('@' + tweet.user + ': ' + tweet.message + '\ Time: ' + tweet.created_at);
+//       $tweet.appendTo('.row2').hide().fadeIn(1000);
+//     }
+
+//     if (tweet.user === 'mracus') {
+
+//       $tweet.text('@' + tweet.user + ': ' + tweet.message + '\ Time: ' + tweet.created_at);
+//       $tweet.appendTo('.row3').hide().fadeIn(1000);
+//     }
+
+//     if (tweet.user === 'douglascalhoun') {
+
+//       $tweet.text('@' + tweet.user + ': ' + tweet.message + '\ Time: ' + tweet.created_at);
+//       $tweet.appendTo('.row4').hide().fadeIn(1000);
+//     }
+
+
+//       index -= 1;
+
+//   }
+
+// });
+
+
+
+
 $(document).ready(function() {
+
   // var $body = $('body');
   // $body.html('');
+  var startIndex;
+
+  getNewTweets(startIndex);
+
+
+
+
+
+});
+
+
+
+
+
+function getNewTweets(startIndex) {
 
   var index = streams.home.length - 1;
-  while (index >= 0) {
+
+  if (!startIndex) {
+    startIndex = 0;
+  }
+
+  console.log('This is the startIndex: ',startIndex);
+  console.log('This is the index: ',index);
+
+  while (index >= startIndex) {
     var tweet = streams.home[index];
     // var $tweet = $('<div></div>');
-    var $tweet = $('<div />', {'class': 'tweet'});
+    var $tweet = $('<div />', {
+      'class': 'tweet'
+    });
 
     // make another, use time div / class to style the time $time = $('<div />', {'class': 'time'});
     // appendTo row4 after the tweet
 
     if (tweet.user === 'shawndrost') {
 
+
+
       $tweet.text('@' + tweet.user + ': ' + tweet.message + '\ Time: ' + tweet.created_at);
       $tweet.appendTo('.row1').hide().fadeIn(1000);
+
+      // append to pop up row that keeps track of all tweets
+      // replace the display row with the new tweets
+
+      // $('.row1').empty();
+      // $tweet.appendTo('.row1').hide().fadeIn(1000);
+      // $tweet.appendTo('#my_popup1');
+
+
     }
 
     if (tweet.user === 'sharksforcheap') {
@@ -57,9 +142,25 @@ $(document).ready(function() {
     }
 
 
-      index -= 1;
+    index -= 1;
+
 
   }
 
-});
+  // var startIndex = streams.home.length;
 
+  startIndex = streams.home.length;
+
+
+
+
+
+
+
+
+
+  setTimeout(function() {
+    getNewTweets(startIndex)
+  }, 10000)
+
+}
